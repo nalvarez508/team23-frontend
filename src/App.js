673cea3407610ad787e9616e0c213b2babb149e2
Harrison Hong <harrisonhong14@gmail.com>
2020-12-07 23:02:20 -0800

    Fixed App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
  
-//import AddItem from './AddItem/add';
 import InventoryList from './InventoryList/inventoryList';
-import Home from './Home';
+import Home from './Home/Home';
+import AddItem from './AddItem/add';
 
 class App extends Component {
   render() {
     return (      
-      
-
       <BrowserRouter>
        <div>
-         
            <Switch>
             <Route path="/" component={Home} exact/>
+            <Route path="/AddItem" component={AddItem}/>
             <Route path="/InventoryList" component={InventoryList}/>
           </Switch>
        </div> 
