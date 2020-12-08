787b7bc9c8c627a9b641f6e50416d965f8dfa85b
D2Tron <jayamsutariya26@gmail.comn>
2020-12-08 03:42:35 -0800

    FInal InventoryList commit before evaluation

--- src/InventoryList/inventoryList.js
+++ src/InventoryList/inventoryList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import * as ReactBootStrap from "react-bootstrap";
+import './inventoryList.css';
 
 const InventoryList = () => {
     const items = [
         {name: "Whipped Cream", quantity: "1 puff", sku: "001", available: 550, incoming: 0},
-        {name: "Whipped Cream", quantity: "1 puff", sku: "001", available: 550, incoming: 0},
-        {name: "Whipped Cream", quantity: "1 puff", sku: "001", available: 550, incoming: 0},
-        {name: "Whipped Cream", quantity: "1 puff", sku: "001", available: 550, incoming: 0}
+        {name: "Sugar", quantity: "1 gram", sku: "002", available: 984, incoming: 0},
+        {name: "Coffee Beans", quantity: "100 grams", sku: "003", available: 1587, incoming: 0},
+        {name: "Chocolate Syrup", quantity: "1 oz", sku: "004", available: 58, incoming: 128}
     ]
 
     const renderItems = (item, index) => {
@@ -23,9 +24,13 @@ const InventoryList = () => {
 
     return(
         <div ClassName="InventoryList">
-            <header>
-                <h1><stim>STIM</stim></h1>
-                <h1>Inventory List</h1>
+            <header class = "top">
+                <table class = "header">
+                    <tr>
+                        <td class><h1>STIM</h1></td>
+                        <td class = "header"><h1>Inventory List</h1></td>
+                    </tr>
+                </table>
             </header>
 
             <ReactBootStrap.Table striped bordered hover>
