2aa23f1a6f6f9f492f059a08e62add9ad870eb96
Harrison Hong <harrisonhong14@gmail.com>
2020-12-07 23:42:33 -0800

    Created Subitem

---
+++ src/SubItem/subItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const subItem = () =>{
+    return (
+        <div>
+            <h1>Add Sub Item</h1>
+                <form>
+                    <p>Name </p>
+                    <input
+                    type="text"/>
+                    </form>
+                <p>Price/Amount</p>
+                <input
+                    type="text"/>
+                <p>SKU</p>
+                <input
+                    type="text"/>
+                <p>Min. Usable Quantity</p>
+                <input
+                    type="text"/>
+                <button onClick = {Submit}>Submit</button>
+        </div>
+        );
+}
+
+export default subItem;
