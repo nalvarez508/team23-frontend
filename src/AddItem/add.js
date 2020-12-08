025b39f640c300b4cf577b5c7f20d9a3e298116a
D2Tron <jayamsutariya26@gmail.comn>
2020-12-08 03:41:39 -0800

    Added headers to add file

--- src/AddItem/add.js
+++ src/AddItem/add.js
@@ -5,6 +5,14 @@ import React from 'react';
 const addItem = () =>{
     return (
         <div>
+            <header class = "top">
+                <table class = "header">
+                    <tr>
+                        <td class><h1>STIM</h1></td>
+                        <td class = "header"><h1>Add Items</h1></td>
+                    </tr>
+                </table>
+            </header>
             <h1>Add Main Item</h1>
                 <form>
                     <p>Name </p>
