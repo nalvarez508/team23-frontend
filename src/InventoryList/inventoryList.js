5061d2e7cce9b409603e8352d551b0a7fbff394d
D2Tron <jayamsutariya26@gmail.com>
2020-12-07 19:41:55 -0800

    Routing components added

--- src/InventoryList/inventoryList.js
+++ src/InventoryList/inventoryList.js
@@ -1,36 +1,41 @@
 import Table from 'react-bootstrap/Table'
+import React from 'react';
 
-<Table striped bordered hover>
-    <thead>
-        <tr>
-            <th>Item</th>
-            <th>Quantity</th>
-            <th>SKU</th>
-            <th>Available</th>
-            <th>Incoming</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>Whipped Cream</td>
-            <td>1 puff</td>
-            <td>001</td>
-            <td>550</td>
-            <td>0</td>
-        </tr>
-        <tr>
-            <td>Sugar</td>
-            <td>1 gram</td>
-            <td>002</td>
-            <td>550</td>
-            <td>0</td>
-        </tr>
-        <tr>
-            <td>Coffee Beans</td>
-            <td>100 grams</td>
-            <td>003</td>
-            <td>1587</td>
-            <td>0</td>
-        </tr>
-    </tbody>
-</Table>
+const inventoryList = () => {
+    <Table striped bordered hover>
+        <thead>
+            <tr>
+                <th>Item</th>
+                <th>Quantity</th>
+                <th>SKU</th>
+                <th>Available</th>
+                <th>Incoming</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Whipped Cream</td>
+                <td>1 puff</td>
+                <td>001</td>
+                <td>550</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>Sugar</td>
+                <td>1 gram</td>
+                <td>002</td>
+                <td>550</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>Coffee Beans</td>
+                <td>100 grams</td>
+                <td>003</td>
+                <td>1587</td>
+                <td>0</td>
+            </tr>
+        </tbody>
+    </Table>
+}
+
+export defualt inventoryList;
