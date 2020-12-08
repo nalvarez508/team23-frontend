fc35414b5f0db29d4a09381ff4a7760f0c4da009
D2Tron <jayamsutariya26@gmail.comn>
2020-12-08 03:41:59 -0800

    Added headers and navigation bar

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,38 +1,35 @@
 import React from 'react';
-//import './Home.css';
+import './Home.css';
 
 const home = () =>{
     return(
         <div>
-            <header>
-            <h1><stim>
-            STIM
-            </stim>
-            
-            </h1>
-            <h1>
-            Your Dashboard
-            </h1>
-        </header>
+            <header class = "top">
+                <table class = "header">
+                    <tr>
+                        <td class><h1>STIM</h1></td>
+                        <td class = "header"><h1>Dashboard</h1></td>
+                    </tr>
+                </table>
+            </header>
         <div/>
 
       <body>
         <header>
           
-            <div>
-                <nav>
-                  <ul>
-                    <li><a href= "/">Home</a></li>
-                    <li><a href="/InventoryList">Inventory</a></li>
-                    <li><a href= "#">Alerts</a></li>
-                    <li><a href="/AddItem">Add Item</a></li>
-                    <li><a href= "/SubItem">Add SubItem</a></li>
-                    <li><a href= "#">Ordering</a></li>
-                    <li><a href= "#">Metrics</a></li>
-                    <li><a href="#">Settings</a></li>
-                  </ul>
-                </nav>
+            <div class = "menu">
+                <ul>
+                  <li><a class="active" href= "/">Home</a></li>
+                  <li><a href="/InventoryList">Inventory</a></li>
+                  <li><a href= "#">Alerts</a></li>
+                  <li><a href="/AddItem">Add Item</a></li>
+                  <li><a href= "/SubItem">Add SubItem</a></li>
+                  <li><a href= "#">Ordering</a></li>
+                  <li><a href= "#">Metrics</a></li>
+                  <li><a href="#">Settings</a></li>
+                </ul>
             </div>
+
         </header>
       </body>
     </div>
