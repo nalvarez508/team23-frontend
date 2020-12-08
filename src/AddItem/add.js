0da12d5ca8b701fad295ceec0afe5306da4e2e1b
Harrison Hong <harrisonhong14@gmail.com>
2020-12-07 23:03:17 -0800

    Added content to add page

--- src/AddItem/add.js
+++ src/AddItem/add.js
@@ -1,8 +1,41 @@
-class React extends React.Component {
+import React from 'react';
+//import './add.css';
+
+class NameForm extends React.Component{
     constructor(props){
-        super(props);
-        this.state = {
-            name: 
+        this.state = {value: ''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event){
+        this.setState({value: event.target.value});
+    }
+    handleSubmit(event){
+        alert('Submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+
+    const addItem = () =>{
+        render(){
+            return (
+                <div>
+                    <h1>Add Main Item</h1>
+                        <p>Name </p>
+                            <form onSubmit={this.handleSubmit} >
+                                <label>
+                                    Name:
+                                    <intput type="text" value ={this.state.value} onChange ={this.handleChange}/>
+                                </label>
+                                <input type='submit' value='Submit' />
+                            </form>
+                        <p>Price</p>
+                        <p>SKU</p>
+                </div>
+
+                );
         }
     }
 }
+
+export default addItem;
