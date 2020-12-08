8c22bfdd49fcedd3ac7628c3aca419b9b4c63468
D2Tron <jayamsutariya26@gmail.com>
2020-12-07 19:16:39 -0800

    Initial Commit + Table creation

---
+++ src/InventoryList/inventoryList.js
@@ -0,0 +1,36 @@
+import Table from 'react-bootstrap/Table'
+
+<Table striped bordered hover>
+    <thead>
+        <tr>
+            <th>Item</th>
+            <th>Quantity</th>
+            <th>SKU</th>
+            <th>Available</th>
+            <th>Incoming</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Whipped Cream</td>
+            <td>1 puff</td>
+            <td>001</td>
+            <td>550</td>
+            <td>0</td>
+        </tr>
+        <tr>
+            <td>Sugar</td>
+            <td>1 gram</td>
+            <td>002</td>
+            <td>550</td>
+            <td>0</td>
+        </tr>
+        <tr>
+            <td>Coffee Beans</td>
+            <td>100 grams</td>
+            <td>003</td>
+            <td>1587</td>
+            <td>0</td>
+        </tr>
+    </tbody>
+</Table>
