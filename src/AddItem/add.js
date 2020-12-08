7bd540c3ca37b86c0b69bbe853e5391304ed407a
Harrison Hong <harrisonhong14@gmail.com>
2020-12-07 23:31:13 -0800

    Added name, price, sku, text box, and subitem header

--- src/AddItem/add.js
+++ src/AddItem/add.js
@@ -1,41 +1,25 @@
 import React from 'react';
 //import './add.css';
 
-class NameForm extends React.Component{
-    constructor(props){
-        this.state = {value: ''};
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleChange(event){
-        this.setState({value: event.target.value});
-    }
-    handleSubmit(event){
-        alert('Submitted: ' + this.state.value);
-        event.preventDefault();
-    }
 
-
-    const addItem = () =>{
-        render(){
-            return (
-                <div>
-                    <h1>Add Main Item</h1>
-                        <p>Name </p>
-                            <form onSubmit={this.handleSubmit} >
-                                <label>
-                                    Name:
-                                    <intput type="text" value ={this.state.value} onChange ={this.handleChange}/>
-                                </label>
-                                <input type='submit' value='Submit' />
-                            </form>
-                        <p>Price</p>
-                        <p>SKU</p>
-                </div>
-
-                );
-        }
-    }
+const addItem = () =>{
+    return (
+        <div>
+            <h1>Add Main Item</h1>
+                <form>
+                    <p>Name </p>
+                    <input
+                    type="text"/>
+                    </form>
+                <p>Price</p>
+                <input
+                    type="text"/>
+                <p>SKU</p>
+                <input
+                    type="text"/>
+                <p>SubItems</p>
+        </div>
+        );
 }
 
 export default addItem;
