7cc14f70acc1b04b5945f24f9e70dc2398feaf39
Harrison Hong <harrisonhong14@gmail.com>
2021-03-21 14:16:43 -0700

    Updated Working Inventory List

--- src/InventoryList/inventoryList.js
+++ src/InventoryList/inventoryList.js
@@ -7,7 +7,7 @@ class InventoryList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            inventory: []
+            data: []
         }
     }
 
@@ -21,8 +21,8 @@ class InventoryList extends Component {
                 "Content-Type": "application/json",
                 "Accept": "application/json",
                 "Access-Control-Allow-Origin": "*",
-                "access_token": "eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..8uvvk6RmuJ0COEM9upnshQ.iuA2BnqYh-LtgsOYPUsyfaLQhe9zsbZ3bqASrOJiCOTATR0-z2T33zfH9Wp8P8NoTjfL1x8zZENeJP4BJrjyDeSIX4PjqbIkxykvAMNqUBdRr8QHfpsXaCYHDYhW76sQCSinuX3im7_9IsKxorv7nZrx-o2z3WS-jalreoAQbYaH_mKyVjeyw6KbpprF-wsjz2JV_98O68Ths0rBP-meJ85I6dGcCtZ-Bk_al17UYZdcUpIC2xK_DRco04W7JAR3JwnsC_W9LHYebJxIOfeq4oiuqSU-tlLEkTgKe09vfWDcEhp7SKEq3bLT0wZHG7EtYQ5csTz9iVJh3xdN_exjUBsnz04LcsaFmkIb347pSCnMiZ8tY4D9UFP_F1ilmHYdqYcT0DZrWh1hEcp5d2WrCC3_ZS9nSOBWA2eheOFO-0WAtKcrQKtNTUjWQbVe7Abnq71fRmzOoNyORdaGmY2CPAEFljlnROlzCJOD6WeszF2OboDdoRXPS2nd6bkSNBbUOmYrEIFE7P7J-VEZbzZh1OTErjeG8aTFbCf9y7rC83LEM28tTqOXPoe6bYCylpw83chdrycGthb5eed_fvMwgsCCVYVQmTzDCjMkyQ8Hk5ZpCt-H5L7AYDgI2o_1cH_NNYJ90TWr0jLbAnCtYr1m6IsxHdBkShgvYWmvI3VTonYEpbW27kcMpsHLfK6mLduvB7g_fSxK1ITqPqIVMWyrle8xlneifUs67BO7p0-mUtJB84-jRRdrMR6B9NMvt8Mm.IVXqEBDYjMntS2nEVZdjBw",
-                "realm_id": "46208163651514239600"
+                "access_token": "eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..Q__3HnUamcJLQSTGixFhAw.oqNiLgvQrIaSiWQpK-FIwtXtA9rtuLLmbWBfEstoibgzgeWFz7P5MqOTp--I2Esdql7DdI-9DlRkLo7nsNPlPLJ5E1fhPSn_uaNtJAaMhauuPFiCd4IPp0k9toLLm1PvfG3rihCtQmTRDZdz5ORtwir42bBevhOocsHUNwF0JOJx4rxM_o3572N61kH-CKU_TNus2ODiw8C8v6u_xnsfFFGdiknOUSaHE6qAwN4mkqITQu9rkQqhDeYCwP84dGlifPTMLniQN83ZI-ImWtgr8F6o3M81VyFUQhFeyWn-UO6Lc2IxT-nl0MM6vlKwIZ3lDmMtKPUaGawTHgeH2IJYK21gJZmxrlBlvJThnkw74HUBHwBE44ACDBbNbc9uyEUCxs9-gOrQcohS_9_R_oQ6bFIh4b-5IICzOMRpZj8I7MTFlWjCOPQgPcIMH5Y20sRNoFltas9TwVUhBFZoax5jYGP4UB3ZhFg9O0fBg3omlOohjlaPgzHXVRumGMllDW1xMvMDMyJHNIyKOTQHdWqBmpuJkQBZ2ePaQLI2N8lY7X4_euooBMBSbTqj9HoUaA8gwawLQA01U35y__1lg8paPwGfo1yr_TM5SHWT-zbMoKJCWkf7z9CUJe7_LDOb7MynUJZqfDMxPrT_fUHM2_AyTOsCuSTb49Q_UkfA5kjbaoehb88i2BohPZSwTnT2Xuz-5j58p75wpmI3bbToBGg2LrFVOYgVcuxyBNDVWsFLEPiS8M3o3LDssFPIpvt3ba9x.4znEXfrwEEPPqgWVBRNRLA",
+                "realm_id": "4620816365151423960"
             }),
         })
 
@@ -60,9 +60,7 @@ class InventoryList extends Component {
 
                 <div className="mainContent">
                     <Table inventory={this.state.data} />
-                    <pre>
-                        {JSON.stringify(this.state.data, null, 2)}
-                    </pre>
+                    
                 </div>
             </body>
     );
