6664b1a7a9c70c7d5b2e12c232debc7fdb80ecaa
Harrison Hong <harrisonhong14@gmail.com>
2021-03-30 01:53:02 -0700

    Automatic subitem token

--- src/SubItem/SubForm.js
+++ src/SubItem/SubForm.js
@@ -21,11 +21,43 @@ function SubForm(){
     const [inputFields, setInputField] = useState([
         {name: '', sku: '', price: '', currentQuantity: ''},
     ]);
+    const [header, setHeader] = useState("");
+
+
+    
 
     const handleSubmit = (e) => {
         e.preventDefault();
         alert('Subitem Added!')
         //console.log("InputFields", inputFields);
+        fetch("http://localhost:8080/getHeaders", {
+            method: 'GET',
+            mode: 'cors',
+            headers: new Headers({
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            }),
+        })
+        .then(res => res.json())
+        .then((resText) => {
+            console.log(JSON.stringify(resText));
+            setHeader(JSON.stringify(resText));
+        })
+        .catch(error => {
+            console.log("Oops!")
+        });
+
+        var token = header;
+        token = token.replace("access_token", '').replace("refresh_token", '').replace("realm_id", '');
+        token = token.replace(/[""]/g, '').replace(":", '').replace(":", '').replace(":", '').replace(/[{}]/g, '');
+        var splitToken = token.split(",");
+        var access = splitToken[0];
+        var id = splitToken[2];
+
+
+
+
         var sentence = JSON.stringify(inputFields);
         //console.log(sentence);
 
@@ -49,8 +81,8 @@ function SubForm(){
         "Content-Type": "application/json",
         "Accept": "application/json",
         "Access-Control-Allow-Origin": "*",
-        "access_token": "eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..Q__3HnUamcJLQSTGixFhAw.oqNiLgvQrIaSiWQpK-FIwtXtA9rtuLLmbWBfEstoibgzgeWFz7P5MqOTp--I2Esdql7DdI-9DlRkLo7nsNPlPLJ5E1fhPSn_uaNtJAaMhauuPFiCd4IPp0k9toLLm1PvfG3rihCtQmTRDZdz5ORtwir42bBevhOocsHUNwF0JOJx4rxM_o3572N61kH-CKU_TNus2ODiw8C8v6u_xnsfFFGdiknOUSaHE6qAwN4mkqITQu9rkQqhDeYCwP84dGlifPTMLniQN83ZI-ImWtgr8F6o3M81VyFUQhFeyWn-UO6Lc2IxT-nl0MM6vlKwIZ3lDmMtKPUaGawTHgeH2IJYK21gJZmxrlBlvJThnkw74HUBHwBE44ACDBbNbc9uyEUCxs9-gOrQcohS_9_R_oQ6bFIh4b-5IICzOMRpZj8I7MTFlWjCOPQgPcIMH5Y20sRNoFltas9TwVUhBFZoax5jYGP4UB3ZhFg9O0fBg3omlOohjlaPgzHXVRumGMllDW1xMvMDMyJHNIyKOTQHdWqBmpuJkQBZ2ePaQLI2N8lY7X4_euooBMBSbTqj9HoUaA8gwawLQA01U35y__1lg8paPwGfo1yr_TM5SHWT-zbMoKJCWkf7z9CUJe7_LDOb7MynUJZqfDMxPrT_fUHM2_AyTOsCuSTb49Q_UkfA5kjbaoehb88i2BohPZSwTnT2Xuz-5j58p75wpmI3bbToBGg2LrFVOYgVcuxyBNDVWsFLEPiS8M3o3LDssFPIpvt3ba9x.4znEXfrwEEPPqgWVBRNRLA",
-        "realm_id": "4620816365151423960"        
+        "access_token": access,
+        "realm_id": id        
         }),
         })
         .then(response => response.json())
