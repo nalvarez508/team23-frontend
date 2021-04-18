7499b1e503b94b35f99fea3c6cdd73b21ac6cf14
Harrison Hong <harrisonhong14@gmail.com>
2021-04-17 23:10:43 -0700

    Cleaned up subitem

--- src/SubItem/subItem.js
+++ src/SubItem/subItem.js
@@ -4,7 +4,6 @@ import SubForm from './SubForm.js'
 class SubItem extends Component{
 
 
-
     onSubmit = (evt) => {
         console.log('The User has submitted Sub Item');
     };
@@ -30,37 +29,9 @@ class SubItem extends Component{
                             <h1>Add Item</h1>
                         </div>
                 </div>
-
-                    {/*<div class = "form">
-                        <form>
-                            <p>Name </p>
-                            <input
-                            type="text"/>
-                            </form>
-                        <p>Price/Amount</p>
-                        <input
-                            type="text"/>
-                        <p>SKU</p>
-                        <input
-                            type="text"/>
-                        <p>Min. Usable Quantity</p>
-                        <input
-                            type="text"/>
-                        <p></p>
-                        <button
-                        name='button-1'
-                        value='Submit'
-                        onClick={this.onSubmit}
-                        >
-                            Submit
-                    </button>
-                </div>
-                */}
                     <div class = "boxes">
                         <SubForm/>
                     </div>
-
-
             </body>
             );
         }
